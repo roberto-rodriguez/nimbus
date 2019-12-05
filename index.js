338f2830a3f5dc82ff69b80a64b7e65b3ee14f2e
Roberto Rodriguez <roberto.rodriguez@trustvesta.com>
2019-12-05 16:38:05 -0500

    Adding prod clause

--- index.js
+++ index.js
@@ -28,5 +28,18 @@ require("./server/routes/auth.routes")(app);
 require("./server/routes/cards.routes")(app);
 require("./server/routes/stripe.routes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  // Express  will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
+  //Finally-  Express look if the request is for an asset
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
